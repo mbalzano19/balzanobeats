379de44c9fe17c7ec73ae2e73e6e5c2a451a1216
mbalzano19 <125160521+mbalzano19@users.noreply.github.com>
2023-07-04 17:37:12 -0400

    updates server for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const cors = require('cors')
 require('dotenv').config()
 require('./config/database')
 
@@ -22,6 +23,10 @@ app.use(express.json());
  // to serve from the production 'build' folder
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://balzanobeats.onrender.com']
+}))
 
 // middleware to check and verify a JWT
 // assign user object from the JWT to req.user
